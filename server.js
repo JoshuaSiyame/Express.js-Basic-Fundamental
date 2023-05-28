c902f5008824f71d9757b790184434038ddee95e
joshcodingenv <josh2023decoding@gmail.com>
2023-05-28 07:52:38 +0300

    Connection to Database

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 // import required modules/packages
 const express = require("express");
 const http = require("http");
+const mongoose = require("mongoose");
+
+// configuring database connection
+mongoose.set("strictQuery", true);
+mongoose.connect("mongodb://127.0.0.1:27017/express-js-basic-fundamentals").then(()=>{
+    console.log("connected to database");
+}).catch((err)=>{
+    console.log("Failed to connect to database", err);
+});
 
 // port number to listen to
 const port = 3000;
@@ -10,14 +19,15 @@ const app = express();
 
 // app configurations
 
-// app endpoints
-app.get("/test", function(request, response){
-    response.send("Test route working");
-});
+// commented this, since i have already setup routes from another directory
+// // app endpoints
+// app.get("/test", function(request, response){
+//     response.send("Test route working");
+// });
 
-app.get("/", (req, res)=>{
-    res.send("Welcome home, Dev ");
-});
+// app.get("/", (req, res)=>{
+//     res.send("Welcome home, Dev ");
+// });
 
 // server instance
 const server = http.createServer(app);
